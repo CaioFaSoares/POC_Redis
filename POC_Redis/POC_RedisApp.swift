668f18728aac0089614ae4186f1a49975555e5d2
Caio <caiofasoares@gmail.com>
2023-02-10 18:54:18 -0300

    Initial Commit

---
+++ POC_Redis/POC_RedisApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_RedisApp.swift
+//  POC_Redis
+//
+//  Created by Caio Soares on 10/02/23.
+//
+
+import SwiftUI
+
+@main
+struct POC_RedisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
